91209d46582d7b83a68449378c359face9b2da47
Ion Stici <ionsticidev@gmail.com>
2023-01-16 14:06:09 +0200

    Add js form function

--- script.js
+++ script.js
@@ -2,6 +2,20 @@
 
 const message = document.querySelector(".form__message");
 
-window.addEventListener("keypress", function (e) {
-    message.style.transform = "translateX(0)";
-});
+const form = document.querySelector(".form");
+const btn = document.querySelector(".form-btn");
+
+const submit = function (e) {
+    e.preventDefault();
+
+    document.documentElement.style.setProperty("--opacity", "0");
+    document.documentElement.style.setProperty("--visibility", "hidden");
+    document.documentElement.style.setProperty("--events", "none");
+
+    message.style.opacity = "1";
+    message.style.visibility = "visible";
+    message.style.pointerEvents = "revert";
+};
+
+form.addEventListener("submit", submit);
+btn.addEventListener("click", submit);
